c00e98ad5e3093b39e45cf0ae50a6966b769651d
ayazhanna <83605659+ayazhanna@users.noreply.github.com>
2025-02-20 21:06:33 +0200

    Update PLSQL_Day6_Task8.sql

--- PLSQL_Day6_Task8.sql
+++ PLSQL_Day6_Task8.sql
@@ -1,3 +1,4 @@
+--Create anonymous block that would trigger too_many_rows error with proper exception handling.
 DECLARE
   v_student_name teacher_1.user_info.first_name%TYPE;
   l_user_id teacher_1.user_info.user_id%type:=10;
