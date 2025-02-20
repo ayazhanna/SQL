d258a64619b551b8e44aa0896c54f0435c944558
ayazhanna <83605659+ayazhanna@users.noreply.github.com>
2025-02-20 20:33:11 +0200

    SQL_Day5_Task4.sql

--- SQL_Day5_Task4.sql
+++ SQL_Day5_Task4.sql
@@ -1,3 +1,7 @@
+/*
+Write a query which shows teacher names, surnames and the number of days of how long has each of the teachers worked at
+the school, split the respective result in 3 parts: 0-3 years, 3-5 years, 5+ years – show both the numeric result and the according to group.
+*/
 SELECT 
   first_name, last_name,
   ROUND((SYSDATE - date_of_joining), 0) AS days,
