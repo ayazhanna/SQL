05aaeaf962a7f5ea7aeb79d0e15b169800ddae9b
ayazhanna <83605659+ayazhanna@users.noreply.github.com>
2025-02-20 22:09:31 +0200

    Delete sql/PLSQL_Day6_Task1.sql

--- sql/PLSQL_Day6_Task1.sql
+++
@@ -1,6 +0,0 @@
-DECLARE
-      m_first_name VARCHAR(20):='Ayazhan';
-      m_last_name VARCHAR(20):='Sapargaliyeva';
-BEGIN
-      DBMS_OUTPUT.put_line('My name is '||m_first_name||' ' || m_last_name);
-END;
