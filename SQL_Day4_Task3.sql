2caf4732cb47924342b0eabf2311725594688d0d
ayazhanna <83605659+ayazhanna@users.noreply.github.com>
2025-02-20 20:13:59 +0200

    SQL_Day4_Task3.sql

--- SQL_Day4_Task3.sql
+++ SQL_Day4_Task3.sql
@@ -1,3 +1,8 @@
+/*
+Display a list of students and their grades.
+In list, add field that would indicate that the test grade is bad when grade <4, average if the grade is between 5 and 7, 
+and if grade higher than that, then the grade is great.
+*/
 SELECT 
   student_id, 
  test_grade, course_id,
